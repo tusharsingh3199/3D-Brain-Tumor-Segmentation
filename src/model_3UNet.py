8487d829a6a17bd44125a3b9f8b391b289735c35
Aastha singh <thataastha5@gmail.com>
2026-07-14 02:34:00 +0530

    model_3UNet

---
+++ src/model_3UNet.py
@@ -0,0 +1,23 @@
+def Model_3UNet(input_shape=(128, 128, 128, 4), num_classes=4):
+    inputs = tf.keras.Input(input_shape)
+
+    s1 = conv_block(inputs, 16)
+    p1 = tf.keras.layers.MaxPool3D(2)(s1)
+    s2 = conv_block(p1, 32)
+    p2 = tf.keras.layers.MaxPool3D(2)(s2)
+    s3 = conv_block(p2, 64)
+    p3 = tf.keras.layers.MaxPool3D(2)(s3)
+    s4 = conv_block(p3, 128)
+    p4 = tf.keras.layers.MaxPool3D(2)(s4)
+
+    b = conv_block(p4, 256)
+
+    d1 = decoder_block(b, s4, 128)
+    d2 = decoder_block(d1, s3, 64)
+    d3 = decoder_block(d2, s2, 32)
+    d4 = decoder_block(d3, s1, 16)
+
+    outputs = tf.keras.layers.Conv3D(num_classes, kernel_size=1, activation="softmax")(d4)
+    return tf.keras.Model(inputs, outputs, name="3D_U-Net")
+
+
