f87346f06ba080946afeaf135b2977f7425ed241
Tushar Singh <tusharsingh1346@gmail.com>
2026-07-14 20:10:08 +0530

    training pipeline making....

--- scripts/train.py
+++ scripts/train.py
@@ -1,9 +1,5 @@
+import os
 import tensorflow as tf
-import matplotlib.pyplot as plt
-import nibabel as nib
-import numpy as np
-
-from ipywidgets import interact, IntSlider
 
 from src.data.EDA import EDA
 from src.data.loader import patients_dir
@@ -13,16 +9,17 @@ from src.training.DiceLoss import Dice, Loss, callbacks
 from src.training.mri_results import MRI_Results
 from src.training.plots import plot_loss
 
+# Data Preprocess
+Data_Path = r"C:\Users\dell\Documents\Programming Projects\Python\3D Brain tumor Segmentation"
+patients_dir = patients_dir(Data_Path + "\Data", seed=69)
 
-Data_Path = r"C:\Users\dell\Documents\Programming Projects\Python\3D Brain tumor Segmentation\Data"
-patients_dir = patients_dir(Data_Path, seed=69)
-
+# Exploratory Data Analysis
 EDA(patients_dir)
 
+# Data Preperation
 train_dir = patients_dir[:int(len(patients_dir)*0.8)]
 val_dir = patients_dir[int(len(patients_dir)*0.8):int(len(patients_dir)*0.9)]
 test_dir = patients_dir[int(len(patients_dir)*0.9):]
-
 
 train_dataset = (tf.data.Dataset.from_tensor_slices(train_dir).map(tf_load_patient, num_parallel_calls=tf.data.AUTOTUNE)
                   .batch(1).prefetch(tf.data.AUTOTUNE))
@@ -31,12 +28,16 @@ val_dataset = (tf.data.Dataset.from_tensor_slices(val_dir).map(tf_load_patient, 
 test_dataset = (tf.data.Dataset.from_tensor_slices(test_dir).map(tf_load_patient)
                   .batch(1))
 
+# Model Training
+if not "3D_UNet.keras" in os.listdir(Data_Path + "\Models"):
+    model = Model_3UNet()
+    model.compile(optimizer=tf.keras.optimizers.Adam(1e-4), loss=Loss, metrics=[Dice])
+    history = model.fit(train_dataset, validation_data=val_dataset, epochs=1, callbacks=callbacks)
 
-model = Model_3UNet()
-model.compile(optimizer=tf.keras.optimizers.Adam(1e-4), loss=Loss, metrics=[Dice])
-history = model.fit(train_dataset, validation_data=val_dataset, epochs=1, callbacks=callbacks)
+    plot_loss(history)
+    model.evaluate(test_dataset)
+    model.save(Data_Path + r"Model\3D_UNet.keras")
 
-plot_loss(history)
-model.evaluate(test_dataset)
-
-MRI_Results(patients_dir, model)
+# Results
+model = tf.keras.models.load_model(Data_Path + r"\Models\3D_UNet.keras", custom_objects={"Loss": Loss, "Dice": Dice})
+MRI_Results(test_dir, model)
--- src/training/mri_results.py
+++ src/training/mri_results.py
@@ -1,15 +1,14 @@
 import os
-import random
-
+import secrets
 import matplotlib.pyplot as plt
+from matplotlib.widgets import Slider
 import nibabel as nib
 import numpy as np
 import tensorflow as tf
-from ipywidgets import interact, IntSlider
 
 
 def MRI_Results(patients_dir, model):
-    patient_dir = random.choice(patients_dir)
+    patient_dir = secrets.choice(patients_dir)
     images = []
     patient_id = os.path.basename(patient_dir)
 
@@ -18,9 +17,9 @@ def MRI_Results(patients_dir, model):
         volume = (volume - volume.min()) / (volume.max() - volume.min() + 1e-8)
         images.append(volume)
 
-    image = np.stack(images, axis=-1)
-    mask = nib.load(os.path.join(patient_dir, f"{patient_id}_seg.nii")).get_fdata().astype(np.int32)
-    mask[mask == 4] = 3
+    img = np.stack(images, axis=-1)
+    seg = nib.load(os.path.join(patient_dir, f"{patient_id}_seg.nii")).get_fdata().astype(np.int32)
+    seg[seg == 4] = 3
 
     H, W, D, C = (240, 240, 155, 4)
     patch_size = (128, 128, 128)
@@ -43,7 +42,7 @@ def MRI_Results(patients_dir, model):
     for x in xs:
         for y in ys:
             for z in zs:
-                patch = image[x:x+patch_size[0], y:y+patch_size[1], z:z+patch_size[2], : ]
+                patch = img[x:x+patch_size[0], y:y+patch_size[1], z:z+patch_size[2], : ]
                 patch = np.expand_dims(patch, axis=0)
                 pred = model.predict(patch, verbose=0)[0]
 
@@ -53,32 +52,36 @@ def MRI_Results(patients_dir, model):
     prediction /= count_map
     prediction = np.argmax(prediction, axis=-1)
 
-    def view_slice(z):
-        plt.figure(figsize=(8, 6))
-        titles = ["T1","T1CE","T2","FLAIR"]
+    fig, ax = plt.subplots(3, 4, figsize=(12, 6))
+    fig.canvas.manager.set_window_title("MRI Brain Tumor Prediction Results")
+    plt.subplots_adjust(bottom=0.15)
+    imgs1, imgs2, imgs3, mask1, mask2 = [], [], [], [], []
+    z = 75
 
+    for i in range(4):
+        imgs1.append(ax[0, i].imshow(img[i][:, :, z], cmap="gray"))
+        imgs2.append(ax[1, i].imshow(img[i][:, :, z], cmap="gray"))
+        mask1.append(ax[1, i].imshow(np.ma.masked_where(seg[:, :, z] == 0, seg[:, :, z]), cmap="jet", alpha=0.7, vmin=1, vmax=4))
+
+        imgs3.append(ax[1, i].imshow(img[i][:, :, z], cmap="gray"))
+        mask2.append(ax[1, i].imshow(np.ma.masked_where(prediction[:, :, z] == 0, prediction[:, :, z]), cmap="jet", alpha=0.7, vmin=1, vmax=4))
+
+        ax[0, i].axis("off")
+        ax[1, i].axis("off")
+        ax[2, i].axis("off")
+
+    slider = Slider(plt.axes([0.2, 0.05, 0.6, 0.03]), "Slice", 0, img[0].shape[2] - 1, valinit=z, valstep=1)
+
+    def update(val):
+        z = int(slider.val)
         for i in range(4):
+            imgs1[i].set_data(img[i][:, :, z])
+            imgs2[i].set_data(img[i][:, :, z])
+            mask1[i].set_data(np.ma.masked_where(seg[:, :, z] == 0, seg[:, :, z]))
 
-            plt.subplot(3,4,i+1)
-            plt.imshow(image[:,:,z,i], cmap="gray")
-            plt.title(titles[i])
-            plt.axis("off")
+            imgs3[i].set_data(img[i][:, :, z])
+            mask2[i].set_data(np.ma.masked_where(prediction[:, :, z] == 0, prediction[:, :, z]))
+        fig.canvas.draw_idle()
 
-            plt.subplot(3,4,5+i)
-            plt.imshow(image[:,:,z,i], cmap="gray")
-            plt.imshow(np.ma.masked_where(mask[:,:,z]==0, mask[:,:,z]),
-                       cmap="jet", alpha=0.6, vmin=0, vmax=3)
-            plt.title("Ground Truth")
-            plt.axis("off")
-
-            plt.subplot(3,4,9+i)
-            plt.imshow(image[:,:,z,i], cmap="gray")
-            plt.imshow(np.ma.masked_where(prediction[:,:,z]==0, prediction[:,:,z]),
-                       cmap="jet", alpha=0.6, vmin=0, vmax=3)
-            plt.title("Prediction")
-            plt.axis("off")
-
-        plt.tight_layout()
-        plt.show()
-
-    interact(view_slice, z=IntSlider(min=0, max=154, value=64))
+    slider.on_changed(update)
+    plt.show()
