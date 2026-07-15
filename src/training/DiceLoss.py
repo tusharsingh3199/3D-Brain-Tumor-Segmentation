119ca8fee4cbe4878d66555b7991fc076f0a6d8d
Tushar Singh <89573113+tusharsingh3199@users.noreply.github.com>
2026-07-15 22:37:39 +0530

    Update DiceLoss.py

--- src/training/DiceLoss.py
+++ src/training/DiceLoss.py
@@ -1,27 +1,51 @@
 import tensorflow as tf
 
+CLASS_WEIGHTS = tf.constant([0.05, 0.25, 0.35, 0.35], dtype=tf.float32)
+
+def Dice_Class(y_true, y_pred, class_idx):
+    y_true = tf.one_hot(tf.cast(y_true, tf.int32), depth=4)
+    y_pred = tf.one_hot(tf.argmax(y_pred, axis=-1), depth=4)
+    yt = y_true[..., class_idx]
+    yp = y_pred[..., class_idx]
+
+    smooth = 1e-6
+    intersection = tf.reduce_sum(yt * yp, axis=[1,2,3])
+    union = tf.reduce_sum(yt + yp, axis=[1,2,3])
+
+    dice = (2. * intersection + smooth) / (union + smooth)
+    return tf.reduce_mean(dice)
+
+def Dice_NCR(y_true, y_pred):
+    return Dice_Class(y_true, y_pred, 1)
+
+def Dice_ED(y_true, y_pred):
+    return Dice_Class(y_true, y_pred, 2)
+
+def Dice_ET(y_true, y_pred):
+    return Dice_Class(y_true, y_pred, 3)
 
 def Dice(y_true, y_pred):
     y_true = tf.one_hot(tf.cast(y_true, tf.int32), depth=4)
     y_pred = tf.one_hot(tf.argmax(y_pred, axis=-1), depth=4)
-
     smooth = 1e-6
     intersection = tf.reduce_sum(y_true * y_pred, axis=[1,2,3])
     union = tf.reduce_sum(y_true + y_pred, axis=[1,2,3])
 
-    return tf.reduce_mean((2. * intersection + smooth) / (union + smooth))
-
+    dice = (2. * intersection + smooth) / (union + smooth)
+    return tf.reduce_mean(dice)
 
 def Loss(y_true, y_pred):
     ce = tf.keras.losses.SparseCategoricalCrossentropy()(y_true, y_pred)
     y_true = tf.one_hot(tf.cast(y_true, tf.int32), depth=4)
-    y_pred = tf.one_hot(tf.argmax(y_pred, axis=-1), depth=4)
+    y_pred = tf.clip_by_value(y_pred, 1e-6, 1. - 1e-6)
 
     smooth = 1e-6
     intersection = tf.reduce_sum(y_true * y_pred, axis=[1,2,3])
     union = tf.reduce_sum(y_true + y_pred, axis=[1,2,3])
 
-    dice_loss = 1 - tf.reduce_mean((2. * intersection + smooth) / (union + smooth))
+    dice = (2. * intersection + smooth) / (union + smooth)
+    weighted_dice = tf.reduce_sum(dice * CLASS_WEIGHTS) / tf.reduce_sum(CLASS_WEIGHTS)
+    dice_loss = 1.0 - weighted_dice
     return dice_loss + ce
 
 
